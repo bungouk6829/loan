cd25e5419f1e26f8eeb6b0652556b5d1ade9db65
kimbyeonguk <bungouk6829@gmail.com>
2020-12-23 02:46:45 +0900

    ver0.5

--- web/static/web/javascript/menu_5.js
+++ web/static/web/javascript/menu_5.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $(selector).css('opacity', '0.7');
   }
 
-  href = window.location.href.replace("http://127.0.0.1:8000/","");
+  href = window.location.href.replace("http://www.보라파이낸셜대부.com/","");
 
   if (href == "investor"){
     selector = '#content_menu > ul > a:nth-child(1) > li'
